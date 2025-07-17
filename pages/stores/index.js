89b38a15c7022fddd4b0a4c4171b835ea1b210d4
Jon Young <youngt.jonathan>
2025-07-17 10:20:21 -0500

    stores can now be displayed and created by user

--- pages/stores/index.js
+++ pages/stores/index.js
@@ -1,33 +1,41 @@
-import { useEffect, useState } from 'react'
-import Layout from '../../components/layout'
-import Navbar from '../../components/navbar'
-import { StoreCard } from '../../components/store/card'
-import { getStores } from '../../data/stores'
-
+import { useEffect, useState } from "react";
+import Layout from "../../components/layout";
+import Navbar from "../../components/navbar";
+import { StoreCard } from "../../components/store/card";
+import { getStores } from "../../data/stores";
 
 export default function Stores() {
-  const [stores, setStores] = useState([])
+  const [stores, setStores] = useState([]);
 
   useEffect(() => {
-    getStores().then(data => {
-      if (data) {
-        setStores(data)
-      }
-    })
-  }, [])
+    getStores()
+      .then((data) => {
+        const safeData = Array.isArray(data) ? data : data ? [data] : [];
+        setStores(safeData);
+      })
+      .catch((err) => {
+        console.error("Failed to load stores:", err);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   getStores().then(data => {
+  //     if (data) {
+  //       setStores(data)
+  //     }
+  //   })
+  // }, [])
 
   return (
     <>
       <h1 className="title">Stores</h1>
       <div className="columns is-multiline">
-      {
-        stores.map(store => (
+        {stores.map((store) => (
           <StoreCard store={store} key={store.id} />
-        ))
-      }
+        ))}
       </div>
     </>
-  )
+  );
 }
 
 Stores.getLayout = function getLayout(page) {
@@ -36,5 +44,5 @@ Stores.getLayout = function getLayout(page) {
       <Navbar />
       {page}
     </Layout>
-  )
-}
+  );
+};
